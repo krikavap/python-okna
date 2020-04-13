931d0b67f9208026c50d879bf3b6960225665508
krikavap <39693113+krikavap@users.noreply.github.com>
2020-04-13 17:25:43 +0200

    second version - přiřazení radiobutonu ve form

--- demoRadioButton.py
+++ demoRadioButton.py
@@ -1,18 +1,28 @@
 """
 demoRadioButton.py
+ukázka jednoduchého formuláře s radio buttony
+connection je vytvořena v druhé verzi přímo ve formuláři (návázání na metodu dispFare())
+
 """
 from PyQt5 import QtWidgets, uic
 
 class MyForm(QtWidgets.QDialog):
+    """
+    třída formuláře
+    metoda dispFare přiřadí cenu letu dle vybrané varianty cestování a cenu vypíše do labelu labelFare
+    """
     def __init__(self):
         super().__init__()
-        self.dialog = uic.loadUi("demoRadioButton.ui", self)
-        self.dialog.radioButton_FirstClass.toggled.connect(self.dispFare)
-        self.dialog.radioButton_BusinessClass.toggled.connect(self.dispFare)
-        self.dialog.radioButton_EconomyClass.toggled.connect(self.dispFare)
+        self.dialog = uic.loadUi("demoRadioButton.ui", self)    # načtení formuláře
+        #self.dialog.radioButton_FirstClass.toggled.connect(self.dispFare)
+        #self.dialog.radioButton_BusinessClass.toggled.connect(self.dispFare)
+        #self.dialog.radioButton_EconomyClass.toggled.connect(self.dispFare)
         self.show()
 
     def dispFare(self):
+        """ 
+        přiřadí cenu letu do labelFare
+        """
         fare = 0
         if self.dialog.radioButton_FirstClass.isChecked() == True:
             fare = 150
