12413ce34706685be7e2024f549b975a0d44b142
Krikava Pavel <pavel.krikava@cgi.com>
2020-04-19 07:01:06 +0200

    demo of LCd timer

--- demoLCD.py
+++ demoLCD.py
@@ -7,21 +7,21 @@ from PyQt5 import QtWidgets, uic, QtCore
 class MyForm(QtWidgets.QDialog):
     """
     třída formuláře
-    metoda prenes() přidá text do listu jako další položku
-    metoda smaz() smaže vybranou položku z listu
-    metoda smaz_vse() smaže celý list
-    metoda edituj() umožní editaci vybrané položky listu
+    metoda showlcd() zjistí aktuální čas systému a pošle jej k zobrazení na lcd
     """
     def __init__(self):
         super().__init__()
         self.dialog = uic.loadUi("demoLCD.ui", self)    # načtení formuláře
-        timer = QtCore.QTimer(self)
-        timer.timeout.connect(self.showlcd)
-        timer.start(1000)
+        timer = QtCore.QTimer(self)             # přiřazení objektu třídy QTimer
+        timer.timeout.connect(self.showlcd)     # connect na metodu showlcd
+        timer.start(1000)                       # pauza každých 1000 ms
         self.showlcd()
         self.show()
 
     def showlcd(self):
+        """
+        metoda showlcd() zjistí aktuální čas systému a pošle jej k zobrazení na lcd
+        """
         time = QtCore.QTime.currentTime()
         text = time.toString("hh:mm:ss")
         self.lcdNumber.display(text)
