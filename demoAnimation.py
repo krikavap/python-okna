d964f91f316a07b7e29f83850d7c9e30d48fca32
Krikava Pavel <pavel.krikava@cgi.com>
2020-04-27 21:37:31 +0200

    demo animation - move down

---
+++ demoAnimation.py
@@ -0,0 +1,28 @@
+"""
+demoAnimation.py
+zobrazí padající předmět po stisku tlačítka
+využívá QPropertyAnimation class
+
+"""
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QDialog
+from PyQt5.QtCore import QRect, QPropertyAnimation
+
+class Window(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.dialog = uic.loadUi("demoAnimation1.ui", self)    # načtení formuláře
+        self.show()
+
+    def startAnimation(self):
+        self.anim = QPropertyAnimation(self.labelPic, b"geometry")  # vytvoření objektu třídy QPropertyAnimation
+        self.anim.setDuration(1000)         # prodlevy při animaci
+        self.anim.setStartValue(QRect(270,30,80,80))        # startovní pozice
+        self.anim.setEndValue(QRect(270, 30, 620, 620))     # koncová pozice
+        self.anim.start()           # start animace
+def main():
+    app = QApplication([])
+    _window = Window()
+    app.exec()
+
+main()
