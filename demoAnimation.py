670c73866598a5b9092c39bdb402218543f1b98b
Krikava Pavel <pavel.krikava@cgi.com>
2020-04-27 21:51:42 +0200

    another picture animation

--- demoAnimation.py
+++ demoAnimation.py
@@ -18,7 +18,7 @@ class Window(QDialog):
         self.anim = QPropertyAnimation(self.labelPic, b"geometry")  # vytvoření objektu třídy QPropertyAnimation
         self.anim.setDuration(1000)         # prodlevy při animaci
         self.anim.setStartValue(QRect(270,30,80,80))        # startovní pozice
-        self.anim.setEndValue(QRect(270, 30, 620, 620))     # koncová pozice
+        self.anim.setEndValue(QRect(270, 30, 1020, 1020))     # koncová pozice
         self.anim.start()           # start animace
 def main():
     app = QApplication([])
